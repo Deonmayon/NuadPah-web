b6b2be91ed32b3e098271ba35487b32c6a6afff0
riwgiw <riwkoraphan@gmail.com>
2025-02-13 22:10:01 +0700

    Fix Nav

--- src/page/CreateSingle.jsx
+++ src/page/CreateSingle.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Nav from "../components/Nav";
 import IconCom from "../components/IconCom";
-import Navmenu from "../components/Navmenu";
 
 import { Link, useNavigate } from "react-router-dom";
 
@@ -68,7 +67,7 @@ function CreateSingle() {
   return (
     <div className="bg-white w-full h-full min-h-dvh font-kanit">
       <Nav />
-      <Navmenu className="z-50" />
+      
       <div className="mt-[120px] px-2 sm:px-4 md:px-6 lg:px-16 max-w-[1250px] mx-auto h-full flex justify-center">
         <div className="mt-[30px] w-full flex flex-col items-center">
           <div className="h-[70px] w-full flex flex-row items-center">
--- src/page/ReportManage.jsx
+++ src/page/ReportManage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import IconCom from "../components/IconCom";
 import Nav from "../components/Nav";
-import Navmenu from "../components/Navmenu";
 
 import { Link, useNavigate } from "react-router-dom";
 
@@ -95,7 +94,6 @@ function ReportManage() {
   return (
     <div className="bg-white w-full h-full min-h-dvh font-kanit">
       <Nav className="z-20" />
-      <Navmenu />
       <div className="mt-[120px] px-2 sm:px-4 lg:px-6 max-w-[1440px] mx-auto h-full">
         <div className="hidden sm:block my-[30px]">
           <div className="flex justify-start items-center">
